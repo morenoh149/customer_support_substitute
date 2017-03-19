a4fcb9dfb13befa9a033cbee22702b17c211376d
Harry Moreno <morenoh149@gmail.com>
2017-03-19 17:51:33 -0400

    drilling down

--- munge.py
+++ munge.py
@@ -1,2 +1,5 @@
 import pandas
-corporate_messages = pandas.read_csv('./crowdflower-corporate-messaging/Corporate-messaging-DFE.csv', encoding=='iso8859_2')
+messages = pandas.read_csv('./crowdflower-corporate-messaging/Corporate-messaging-DFE.csv', encoding='iso8859_2')
+dialogues = messages[messages.category == 'Dialogue']
+pandas.set_option('max_colwidth', 140)
+link_dialogues = dialogues[dialogues.text.str.match('.*http.*')]
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@ Software that classifies incoming messages as common question or other.
 If it is a common question it will further classify to question it knows a response to or question it does not know response to.
 
 Research into determining feasibility of automating customer support replies on some company's support phone number or twitter.
+
+# Usage
+`$ python3 -i munge.py`
